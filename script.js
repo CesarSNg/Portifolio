1284452c95a3a45189d879317d57da0f24a07d25
CesarSNg <csantanang@gmail.com>
2020-07-03 22:47:21 -0300

    Correção

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
 document.querySelector(".container").classList.toggle("show-menu")
 );
+
+
+
+
+
+/*Teste do botão
+const buttons = document.querySelector(".banner butonn");
+    buttons.forEach(btn => {
+        btn.addEventListener("click", function(e){
+
+            let x = e.clientX - e.target.offsetLeft;
+            let y = e.clientY - e.target.offsetTop;
+
+            let ripples = document.createElement('button');
+            ripples.style.left = x + 'px';
+            ripples.style.top =  y + 'px';
+            this.appendChild(ripples);
+
+            setTimeout (() => {
+                ripples.remove()
+            },1000);
+        })
+    });*/
